8d0bf453a8361a961906addcb34352efb6caab9f
simsa <simsaaav@mail.ru>
2026-02-27 15:56:28 +0300

    fixed styles with pixel perfect, add hover state

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
-new Swiper('.swiper', {
-  loop: true,
-  pagination: {
-    el: '.slider-pagination',
-    clickable: true,
-  },
-  navigation: {
-    nextEl: '.slider-btn--next',
-    prevEl: '.slider-btn--prev',
-  },
-  autoplay: false
-})
-
 Fancybox.bind("[data-fancybox]", {})
 
+document.querySelectorAll('.product-card').forEach((card) => {
+
+  const swiperEl = card.querySelector('.product-card__slider');
+  const paginationEl = card.querySelector('.slider-pagination');
+  const nextBtn = card.querySelector('.slider-btn--next');
+  const prevBtn = card.querySelector('.slider-btn--prev');
+
+  new Swiper(swiperEl, {
+    loop: true,
+
+    navigation: {
+      nextEl: nextBtn,
+      prevEl: prevBtn,
+    },
+
+    pagination: {
+      el: paginationEl,
+      clickable: true,
+    },
+    autoplay: false
+  });
+
+});
